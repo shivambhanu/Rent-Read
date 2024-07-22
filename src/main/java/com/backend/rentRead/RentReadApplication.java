933b0efa95990b008f91d9aa3d750560b5f8958c
Shivam Bhanu <shivambhanu6924@gmail.com>
2024-07-22 21:12:57 +0530

    added api endpoints to the starter class.

--- src/main/java/com/backend/rentRead/RentReadApplication.java
+++ src/main/java/com/backend/rentRead/RentReadApplication.java
@@ -2,12 +2,28 @@ package com.backend.rentRead;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RentReadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentReadApplication.class, args);
 	}
 
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Hello from AUTHENTICATED endpoint!";
+	}
+
+
+	@GetMapping("/admins")
+	@PreAuthorize("hasAuthority('ADMIN')")
+	public String welcomeAdmin() {
+		return "Hello from ADMIN's endpoint!";
+	}
 }
